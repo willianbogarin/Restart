806e778c042f194141cceceffdcc88a3eee140df
willianbogarin <willianbogarin@hotmail.com>
2023-09-19 11:03:45 +0200

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Willian Bogarin Jr on 2023. 09. 19..
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
